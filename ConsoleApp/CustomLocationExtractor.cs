7a96ec8aa8679f766c01a2a6efa5f5ebcdf055d0
Sangit Gurung <tamu.grgsangit@gmail.com>
2020-02-12 18:02:07 +0900

    Initial commit

---
+++ ConsoleApp/CustomLocationExtractor.cs
@@ -0,0 +1,54 @@
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Data;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class TextLocationStrategy : LocationTextExtractionStrategy
+{
+    public List<textChunk> objectResult = new List<textChunk>();
+
+    public override void EventOccurred(IEventData data, EventType type)
+    {
+        if (!type.Equals(EventType.RENDER_TEXT))
+            return;
+
+        TextRenderInfo renderInfo = (TextRenderInfo)data;
+
+        string curFont = renderInfo.GetFont().GetFontProgram().ToString();
+
+        float curFontSize = renderInfo.GetFontSize();
+
+        IList<TextRenderInfo> text = renderInfo.GetCharacterRenderInfos();
+        foreach (TextRenderInfo t in text)
+        {
+            string letter = t.GetText();
+            Vector letterStart = t.GetBaseline().GetStartPoint();
+            Vector letterEnd = t.GetAscentLine().GetEndPoint();
+            Rectangle letterRect = new Rectangle(letterStart.Get(0), letterStart.Get(1), letterEnd.Get(0) - letterStart.Get(0), letterEnd.Get(1) - letterStart.Get(1));
+
+            if (letter != " " && !letter.Contains(' '))
+            {
+                textChunk chunk = new textChunk();
+                chunk.Text = letter;
+                chunk.Rect = letterRect;
+                chunk.FontFamily = curFont;
+                chunk.FontSize = curFontSize;
+                chunk.SpaceWidth = t.GetSingleSpaceWidth() / 2f;
+
+                objectResult.Add(chunk);
+            }
+        }
+    }
+}
+public class textChunk
+{
+    public string Text { get; set; }
+    public Rectangle Rect { get; set; }
+    public string FontFamily { get; set; }
+    public float FontSize { get; set; }
+    public float SpaceWidth { get; set; }
+}
---
+++ ConsoleApp/DocHiglighter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Annot;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace ConsoleApp
+{
+   public class DocHiglighter
+    {
+        string pdfPath = "D:\\Documents\\Tag Numbering Specification.pdf";
+        void SearchAndHighlight(string text)
+        {
+            // initialize pdf writer
+            PdfWriter pdfWriter = new PdfWriter(pdfPath);
+            // initialize pdf document
+            PdfDocument pdfDoc = new PdfDocument(pdfWriter);
+            // initialize document
+            Document pdfDocument = new Document(pdfDoc);
+
+            
+        }
+
+        public virtual void SetTextAnnotaion(String dest)
+        {
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+            //Initialize document
+            Document document = new Document(pdf);
+            Paragraph p = new Paragraph("The example of text markup annotation.");
+            document.ShowTextAligned(p, 20, 795, 1, TextAlignment.LEFT, VerticalAlignment.MIDDLE, 0);
+            //Create text markup annotation
+            PdfAnnotation ann = PdfTextMarkupAnnotation.CreateHighLight(new Rectangle(105, 790, 64, 10), new float[] {
+                169, 790, 105, 790, 169, 800, 105, 800 }).SetColor(ColorConstants.YELLOW).SetTitle(new PdfString("Hello!")).SetContents
+                (new PdfString("I'm a popup.")).SetTitle(new PdfString("iText")).SetRectangle(new PdfArray
+                (new float[] { 100, 600, 200, 100 }));
+            pdf.GetFirstPage().AddAnnotation(ann);
+            //Close document
+            document.Close();
+        }
+
+        public List<int> ReadPdfFile(string fileName, String searthText)
+        {
+            //PdfDocument reader = new PdfDocument(new PdfReader(fileName));
+            //FilteredEventListener listener = new FilteredEventListener();
+            //var strat = listener.AttachEventListener(new TextLocationStrategy());
+            //PdfCanvasProcessor processor = new PdfCanvasProcessor(listener);
+            //processor.ProcessPageContent(reader.GetPage(1));
+            //Console.WriteLine("=== FINDING COORDINATES ===");
+            //string curtPageText = PdfTextExtractor.GetTextFromPage(reader.GetPage(page), strategy);
+
+            //foreach (var d in strat.objectResult)
+            //{
+            //    Console.WriteLine("Char >" + d.Text + " X >" + d.Rect.GetX() + " font >" + d.FontFamily + " font size >" + d.FontSize.ToString() + " space >" + d.SpaceWidth);
+
+            //}
+
+
+            List<int> pages = new List<int>();
+            if (File.Exists(fileName))
+            {
+                using (PdfReader pdfReader = new PdfReader(fileName))
+                using (PdfDocument pdfDocument = new PdfDocument(pdfReader))
+                {
+                    FilteredEventListener listener = new FilteredEventListener();
+                    var strat = listener.AttachEventListener(new TextLocationStrategy());
+                    PdfCanvasProcessor processor = new PdfCanvasProcessor(listener);
+                    for (int page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
+                    {
+                        ITextExtractionStrategy strategy = new TextLocationStrategy();
+                        
+                        processor.ProcessPageContent(pdfDocument.GetPage(page));
+                        string currentPageText = PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(page), strategy);
+
+                        if (currentPageText.ToLower().Contains(searthText.ToLower()))
+                        {
+                            foreach (var d in strat.objectResult)
+                            {
+                                Console.WriteLine("Char >" + d.Text + " X >" + d.Rect.GetX() + " font >" + d.FontFamily + " font size >" + d.FontSize.ToString() + " space >" + d.SpaceWidth);
+
+                            }
+                            pages.Add(page);
+                        }
+                    }
+                }
+            }
+            return pages;
+        }
+
+    }
+}
